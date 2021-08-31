0b018c8f48d8025c7b95b6f9a06285dbbd027ebd
James Gao <392041299@qq.com>
2021-08-31 15:09:20 +0800

    feat: add postcss-px-to-viewport plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,19 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import eslint from 'vite-plugin-eslint';
+import viewport from 'postcss-px-to-viewport';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  css: {
+    postcss: {
+      plugins: [
+        viewport({
+          viewportWidth: 750,
+          exclude: /(\/|\\)(node_modules)(\/|\\)/,
+        }),
+      ],
+    },
+  },
   plugins: [vue(), eslint({ cache: false })],
 });
